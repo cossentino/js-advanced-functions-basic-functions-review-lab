0d54d17b4b072dbfa45051b8568f5622cc35ecde
Ian Cossentino <icossentino@gmail.com>
2021-03-02 11:21:05 -0500

    commit tree cec346569000060f0a9668049543fef9f9d81a8c

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Your code here
+
+
+
+function saturdayFun(activity="roller-skate") {
+  return `This Saturday, I want to ${activity}!`
+}
+
+
+
+const mondayWork = function(str="go to the office") {
+  return `This Monday, I will ${str}.`
+}
+
+// Instantly-invoked function expression (IIFE)
+// We can define an anonymous function expression and then immediately pass it arguments
+
+function wrapAdjective(char="*") { 
+  return function(str='special') {
+    return `You are ${char}${str}${char}!`
+  }
+}
+
+
+const Calculator = {
+  'add': function(a,b) {
+    return a + b
+  },
+  'subtract': function(a,b) {
+    return a - b
+  },
+  'multiply': function(a,b) {
+    return a * b
+  },
+  'divide': function(a,b) {
+    return a / b
+  },
+}
+
+
+
+function actionApplyer(int, funcArray) {
+	let message = "13, multiplied by 2, added to 1000 and then modulo 7 is 4. Apply each function in the Array of functions on the given base (13) OR on the result of the use of the previous function to get this result"
+  for (let i = 0; i < funcArray.length; i++) {
+    int = funcArray[i](int)
+  }
+  return int
+}
